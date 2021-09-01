1b76ea08690ee6fa95a445833f9ae0654fb3f497
Vivek Jadhav <jadhavvivek619@gmail.com>
2021-09-01 19:58:12 +0530

    Add project files.

---
+++ FinalizeEx.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FinalizedDemo
+{
+    class FinalizeEx
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
