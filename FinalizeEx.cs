b705ee5158aa40c9468b9ecfd2c055a484b6e115
Vivek Jadhav <jadhavvivek619@gmail.com>
2021-09-01 20:04:38 +0530

    FIRST COMMIT

--- FinalizeEx.cs
+++ FinalizeEx.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace FinalizedDemo
 {
+    public class FinalizeOperation
+    {
+        Stopwatch sp;
+        public FinalizeOperation()
+        {
+            sp = Stopwatch.StartNew();
+            Console.WriteLine("StopWatch Strted");
+        }
+        public void showDuration()
+        {
+            Console.WriteLine("This instance of {0} has been in existence for {1}", this, sp.Elapsed);
+        }
+        ~FinalizeOperation()
+        {
+            Console.WriteLine("Finalizing object");
+            sp.Stop();
+            Console.WriteLine("This instance of {0} has been in existence for {1}",this, sp.Elapsed);
+        }
+    }
     class FinalizeEx
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            FinalizeOperation FO = new FinalizeOperation();
+            FO.showDuration();
         }
     }
 }
